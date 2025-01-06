118cb7fc8699f68d140e0adbad777f7090a437ce
mubtasimrahman <mubtasim.rahman.mr@gmail.com>
2025-01-06 01:35:16 -0500

    Working version of FeaturedProjectsAdmin

--- client/src/App/AdminApp.tsx
+++ client/src/App/AdminApp.tsx
@@ -1,10 +1,10 @@
-// import AllTradesAdmin from "../Components/AllTrades/AllTradesAdmin";
+import AllTradesAdmin from "../Components/AllTrades/AllTradesAdmin";
 import FeaturedProjectsAdmin from "../Components/FeaturedProjects/FeaturedProjectsAdmin";
 
 const AdminApp = () => {
   return (
-    <div>
-      {/* <AllTradesAdmin></AllTradesAdmin> */}
+    <div className="d-flex flex-column" style={{backgroundColor:"white"}}>
+      <AllTradesAdmin></AllTradesAdmin>
       <FeaturedProjectsAdmin></FeaturedProjectsAdmin>
     </div>
   );
